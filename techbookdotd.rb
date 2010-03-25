ff8b7a8e2a2dd6c049085875894c27b3f09c3c40
Rob Warner <rwarner@rwarner-macbook.(none)>
2010-03-25 08:23:53 -0400

    Fixed Apress cover image

--- techbookdotd.rb
+++ techbookdotd.rb
@@ -17,18 +17,13 @@ def get_deals
   deals << get_oreilly(open('http://feeds.feedburner.com/oreilly/ebookdealoftheday'))
 end
 
-def get_apress1
-  Deal.new(:vendor => 'Apress', :title => 'The Definitive Guide to SWT and JFace',
-    :amount => '9.99', :image_url => 'http://apress.com/resource/bookcover/9781590598344?size=medium',
-    :url => 'http://apress.com/info/dailydeal')
-end
-
 def get_apress(content)
   url_part = /.*\<div class='bookdetails'\>.*?\<a href='(.*?)'\>.*/m.match(content)[1]
   title = /.*\<div class='bookdetails'\>.*?\<a href='.*?'\>(.*?)\<a.*/m.match(content)[1]
+  image_url_part = /.*\<div class='cover'\>.*?\<img.*?src="(.*?)".*/m.match(content)[1]
 
   Deal.new(:vendor => 'Apress', :title => title, :url => "http://apress.com#{url_part}", 
-    :image_url => "http://apress.com/resource/bookcover/#{url_part.split(/\//)[-1]}?size=medium")
+    :image_url => "http://apress.com#{image_url_part}")
 end
 
 def get_manning(content)
