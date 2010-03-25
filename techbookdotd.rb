bcaadd15c7b0fb5980a7bedbb124f14fa110a49d
Rob Warner <rwarner@rwarner-macbook.(none)>
2010-03-24 22:27:16 -0400

    Yeah, I kept saying Manning, but it was really O'Reilly that's implemented

--- techbookdotd.rb
+++ techbookdotd.rb
@@ -12,13 +12,24 @@ end
 
 def get_deals
   deals = Array.new
-  deals << Deal.new(:vendor => 'Apress', :title => 'The Definitive Guide to SWT and JFace',
+  deals << get_apress
+  deals << get_manning
+  deals << get_oreilly
+end
+
+def get_apress
+  Deal.new(:vendor => 'Apress', :title => 'The Definitive Guide to SWT and JFace',
     :amount => '9.99', :image_url => 'http://apress.com/resource/bookcover/9781590598344?size=medium',
     :url => 'http://apress.com/info/dailydeal')
-  deals << get_manning
 end
 
 def get_manning
+  Deal.new(:vendor => 'Manning', :title => 'The Definitive Guide to SWT and JFace',
+    :amount => '9.99', :image_url => 'http://apress.com/resource/bookcover/9781590598344?size=medium',
+    :url => 'http://apress.com/info/dailydeal')
+end
+
+def get_oreilly
   rss = SimpleRSS.parse open('http://feeds.feedburner.com/oreilly/ebookdealoftheday')
   entry = rss.entries.first
 
@@ -28,6 +39,6 @@ def get_manning
 
   amount = /.*\$(.*?)!.*/.match(entry.title)[1]
 
-  Deal.new(:vendor => 'Manning', :title => entry.title, :url => entry.link, :amount => amount,
+  Deal.new(:vendor => "O'Reilly", :title => entry.title, :url => entry.link, :amount => amount,
     :image_url => "http://covers.oreilly.com/images/#{cat_num}/cat.gif")
 end
